423e86e13ac5a6c8a4fdd0d5f25c683461b80b66
Oritsegbubemi <slimnfine22@gmail.com>
2022-03-18 12:19:32 +0100

    --- Auto-Git Commit ---

--- middleware/uploadImage.js
+++ middleware/uploadImage.js
@@ -1,36 +1,39 @@
 const path = require('path')
 const fs = require('fs')
 const multer = require('multer')
+const Grid = require('gridfs-stream')
+const GridFsStorage = require
+
   
 
 
 
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb ) {
-        cb(null, './uploads/')
-    },
-    filename: function (req, file, cb){
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb ) {
+//         cb(null, './uploads/')
+//     },
+//     filename: function (req, file, cb){
     
-     cb(null, Date.now().toISOString() + file.originalname)
-    }
-});
+//      cb(null, Date.now().toISOString() + file.originalname)
+//     }
+// });
 
-const fileFilter = (req, file, cb) => {
-    if(file.minetype === 'image/jpeg' || file.minetype === 'image/png') {
-    cb(null, true)
-} else {
-    cb(null,false)
-  };
-}
+// const fileFilter = (req, file, cb) => {
+//     if(file.minetype === 'image/jpeg' || file.minetype === 'image/png') {
+//     cb(null, true)
+// } else {
+//     cb(null,false)
+//   };
+// }
 
-const upload = multer({
-    storage: storage,
-limits: {
-    fileSize: 1024 * 1024 * 5
-},
- fileFilter: fileFilter
-});
+// const upload = multer({
+//     storage: storage,
+// limits: {
+//     fileSize: 1024 * 1024 * 5
+// },
+//  fileFilter: fileFilter
+// });
 
 
 module.exports = upload;
